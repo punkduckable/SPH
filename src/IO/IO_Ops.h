e0079bc3bd4369317a6f36832ac70d4e1dc1ee44
Robert Stephany <robert.stephany@utexas.edu>
2020-01-20 12:40:28 -0600

    Got rid of clutter
    I got rid of some horrible functions that I wrote to do the whole read/write of Box BC information stuff. Good riddance.

--- src/IO/IO_Ops.h
+++ src/IO/IO_Ops.h
@@ -11,18 +11,11 @@ This file houses several functions that make reading and writing from a file
 easier. */
 
 std::string read_line_after_char(FILE * File, const char delim);
-Vector read_box_BC(std::string BC_Str);
-//void write_box_BC(FILE * File, const Vector & BC_In);
 
 namespace String_Ops {
   bool Contains(const char* Buffer,                                            // Intent: Read
                 const char* Word,                                              // Intent: Read
                 unsigned Search_At = 0);                                       // Intent: Read
-
-  std::vector<std::string> Split(std::string & S,                              // Intent: Read
-                                 const char Delim = ',');                      // Intent: Read
-  std::vector<std::string> Split(const char* S,                                // Intent: Read
-                                 const char Delim = ',');                      // Intent: Read
 } // namespace String_Ops {
 
 #endif
